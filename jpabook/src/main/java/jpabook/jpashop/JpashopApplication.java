eb994dfd3822a1bfbfcbfdcda34468464fe5de15
bgr02 <snin023@naver.com>
2020-12-11 15:29:18 +0900

    add spring jpa adata

---
+++ jpabook/src/main/java/jpabook/jpashop/JpashopApplication.java
@@ -0,0 +1,24 @@
+package jpabook.jpashop;
+
+import com.fasterxml.jackson.datatype.hibernate5.Hibernate5Module;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class JpashopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpashopApplication.class, args);
+	}
+
+	@Bean
+	Hibernate5Module hibernate5Module() {
+		//지연로딩을 강제로 다 조회해서 가져온다.
+		/*Hibernate5Module hibernate5Module = new Hibernate5Module();
+		hibernate5Module.configure(Hibernate5Module.Feature.FORCE_LAZY_LOADING, true);
+		return hibernate5Module;*/
+
+		return new Hibernate5Module();
+	}
+}
